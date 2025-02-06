5ef9032c15593c0e092c083b4904ee50bbea456c
2410991073 <devyani1073.becse24@chitkara.edu.in>
2025-02-07 00:26:12 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    float a, b, c;
+    int  a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
 
-    // Input sides of the triangle
-    printf("Enter the lengths of the three sides of the triangle:\n");
-    scanf("%f %f %f", &a, &b, &c);
-
-    // Check if the sides satisfy the triangle inequality theorem
     if (a + b > c && b + c > a && c + a > b) {
         printf("The sides %.2f, %.2f, and %.2f can form a valid triangle.\n", a, b, c);
     } else {
