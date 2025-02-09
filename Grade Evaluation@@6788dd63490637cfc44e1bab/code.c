6f10e63429e0235e920b16a287c9645224dc502d
2410991073 <devyani1073.becse24@chitkara.edu.in>
2025-02-09 19:40:40 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int c;
+
+    scanf("%c",&c);
+
+    if(c=='A'){
+        printf("Excellent");
+    }
+    else if(c=='B'){
+        printf("Good");
+    }
+    else if(c=='C'){
+        printf("Average");
+    }
+    else if( c=='D'){
+        printf("Below Average");
+    }
+    else{
+        printf("Fail");
+    }
+    return 0;
+
+}
