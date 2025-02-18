8430f60d4acb8b035e507cf5a0948a27c82fe832
2410991073 <devyani1073.becse24@chitkara.edu.in>
2025-02-18 10:37:41 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,30 +1,32 @@
-#include<stdio.h>
-int main(){
-    int a , b ;
+#include <stdio.h>
+
+int main() {
+    int a, b;
     char c;
     int result;
 
-    scanf("%d %d %c ",&a,&b,&c);
+    // Corrected scanf without the trailing space
+    scanf("%d %d %c", &a, &b, &c);
 
-    switch(c){
+    switch (c) {
         case '+':
-        result = a+b;
-        printf("Result: %d\n",result);
-        break;
+            result = a + b;
+            printf("Result: %d\n", result);
+            break;
         case '-':
-        result= a-b;
-        printf("Result: %d\n",result);
-        break;
+            result = a - b;
+            printf("Result: %d\n", result);
+            break;
         case '*':
-        result = a*b;
-        printf("Result: %d\n",result);
-        break;
+            result = a * b;
+            printf("Result: %d\n", result);
+            break;
         case '/':
-        result = a/b;
-        printf("Result: %d\n",result);
-        break;
-        default:
-        printf("error\n");
+            if (b != 0) {
+                result = a / b;
+                printf("Result: %d\n", result);
+            } else {
+                printf("Error: Division")}
     }
-    return 0;
-}   
+}
+
