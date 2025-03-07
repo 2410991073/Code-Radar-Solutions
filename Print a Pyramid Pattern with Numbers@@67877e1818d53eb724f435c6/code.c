545ce7f085d02985882ad82837b30b1a5c2ca439
2410991073 <devyani1073.becse24@chitkara.edu.in>
2025-03-07 17:01:57 +0530

    Solution to Print a Pyramid Pattern with Numbers

--- Print a Pyramid Pattern with Numbers@@67877e1818d53eb724f435c6/code.c
+++ Print a Pyramid Pattern with Numbers@@67877e1818d53eb724f435c6/code.c
@@ -1,40 +1,40 @@
-// #include<stdio.h>
-// int main(){
-//     int N;
-//     int i,j,k;
-//     scanf("%d",&N);
-
-//     for ( i=1;i<=N;i++){
-//         for( k = 1;k = N-i;k++){
-//             printf(" ");
-//         }
-//         for( j = 1;j = 2*i-1; j++){
-//             printf("%d",j);
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
 #include<stdio.h>
-
-int main() {
+int main(){
     int N;
-    int i, j, k;
-    scanf("%d", &N);  
+    int i,j,k;
+    scanf("%d",&N);
 
-    for (i = 1; i <= N; i++) {
-        
-        for (k = 1; k = N - i; k++) {
+    for ( i=1;i<=N;i++){
+        for( k = 1;k = N-i;k++){
             printf(" ");
         }
-        
-
-        for (j = 1; j <= 2 * i - 1; j++) {
-            printf("%d ", j);
+        for( j = 1;j = 2*i; j++){
+            printf("%d",j);
         }
-        
-        printf("\n");  
+        printf("\n");
     }
-
     return 0;
 }
+// #include<stdio.h>
+
+// int main() {
+//     int N;
+//     int i, j, k;
+//     scanf("%d", &N);  
+
+//     for (i = 1; i <= N; i++) {
+        
+//         for (k = 1; k <= N - i; k++) {
+//             printf(" ");
+//         }
+        
+
+//         for (j = 1; j <= 2 * i - 1; j++) {
+//             printf("%d ", j);
+//         }
+        
+//         printf("\n");  
+//     }
+
+//     return 0;
+// }
